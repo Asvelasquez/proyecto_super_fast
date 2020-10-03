bf772bcd8527d4a302f6d7faf9a460cd333d4650
ghostsev77 <32886956+ghostsev77@users.noreply.github.com>
2020-10-03 14:39:25 -0500

    clases
    se agregaron clases de prueba

---
+++ proyecto_super_fast/App_Code/DAO/prueba1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de prueba1
+/// </summary>
+public class prueba1
+{
+    public prueba1()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
---
+++ proyecto_super_fast/App_Code/Encapsulamiento/prueba2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de prueba2
+/// </summary>
+public class prueba2
+{
+    public prueba2()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
