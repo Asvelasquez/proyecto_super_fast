9e32e1f2b25460fae3f35bb0eb45f807b6877db1
ghostsev77 <32886956+ghostsev77@users.noreply.github.com>
2020-10-08 21:53:12 -0500

    calses nuevas
    se agrego cerrar session
    y se arreglo parte del login
    y de registrar

--- proyecto_super_fast/Controller/CerrarSession.aspx.cs
+++ proyecto_super_fast/Controller/CerrarSession.aspx.cs
@@ -5,15 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class View_trabajarnosotros : System.Web.UI.Page
+public partial class View_CerrarSession : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
-    }
-
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-
+        Session["user"] = null;
+        Response.Redirect("Login.aspx");
     }
 }
