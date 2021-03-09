a3dfdb4ae5595c05721a0ee47c9fe8921a582fa3
Ranicaro <nicolascaceresrom@hotmail.com>
2021-03-09 10:16:04 -0500

    migracion

---
+++ Logica/LPerfil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios;
+using Data;
+namespace Logica{
+   public class LPerfil{
+        UMac datos1 = new UMac();
+        public UUsuario mostrar(int id){
+
+         DAOUsuario us = new DAOUsuario();
+         UUsuario usuario1 = new UUsuario();
+           
+         return usuario1 = us.mostrar(id);
+         }
+        //
+        public string mostrar1() {
+          return  datos1.Url=  "AccesoDenegado.aspx";
+        }
+        //
+        public void BTN_guardar(UUsuario usuario1){
+                DAOUsuario us = new DAOUsuario();
+                us.actualizarperfil(usuario1);
+        }//
+
+        //
+    }//class
+}
